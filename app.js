2b7baf85a45a2bf5396804e114e04c858e101002
github3107 <ram.pendem@gmail.com>
2019-08-11 16:04:57 -0400

    added express

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-const http = require('http');
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write(`<h1>Node.js APP running at http://${hostname}:${port}/</h1>`);
-  res.end('<h2>Hello World</h2>\n');
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-
-server.listen(port, ip, () => {
-  console.log(`Server running at http://${ip}:${port}/`);
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
 });
